9584b2b216661e2c3c71ceffa9b65eb9bee0d7df
nhlee98 <e5slnh9876@gmail.com>
2023-11-14 02:00:41 +0900

    feat: 총주문 금액이 10,000원 이상인 경우에만 이벤트를 적용하는 기능 구현

--- src/main/java/christmas/domain/event/Discounts.java
+++ src/main/java/christmas/domain/event/Discounts.java
@@ -2,22 +2,28 @@ package christmas.domain.event;
 
 import christmas.domain.Money;
 import christmas.domain.Reservation;
+import christmas.domain.event.rule.EventRule;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Discounts {
     private final List<Discount> discounts;
+    private final EventRule commonRule;
 
-    public Discounts(List<Discount> discounts) {
+    public Discounts(List<Discount> discounts, EventRule commonRule) {
         this.discounts = new ArrayList<>(discounts);
+        this.commonRule = commonRule;
     }
 
     public Map<Discount, Money> discount(Reservation reservation) {
+        if (!commonRule.isSatisfiedBy(reservation)) {
+            return Collections.emptyMap();
+        }
+
         return discounts.stream()
+                .filter(discount -> !discount.discount(reservation).equals(Money.zeroInstance()))
                 .collect(Collectors.toMap(Function.identity(), discount -> discount.discount(reservation)));
     }
 }
