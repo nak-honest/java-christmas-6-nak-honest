e2670092fc6203d38302e9cfcceed14202142532
nhlee98 <e5slnh9876@gmail.com>
2023-11-14 19:34:17 +0900

    fix: 여러개의 대시로 구분했을때 인덱스 에러가 나는 버그 해결

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,7 +1,5 @@
 package christmas.view;
 
-import org.assertj.core.data.MapEntry;
-
 import static christmas.ErrorMessage.INVALID_DAY_ERROR;
 import static christmas.ErrorMessage.INVALID_MENU_ERROR;
 
@@ -9,7 +7,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 public class InputView {
     private static final String DAY_INPUT_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
@@ -17,6 +14,9 @@ public class InputView {
             "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
     private static final String ORDER_ITEM_DELIMITER = ",";
     private static final String ORDER_MENU_COUNT_DELIMITER = "-";
+    private static final int MENU_ITEM_INDEX_COUNT = 2;
+    private static final int MENU_NAME_INDEX = 0;
+    private static final int MENU_COUNT_INDEX = 1;
 
     private final Supplier<String> reader;
     private final Writer writer;
@@ -35,27 +35,36 @@ public class InputView {
         }
     }
 
-    public List<Map.Entry<String, Integer>> readMenus() {
+    public List<Map.Entry<String, Integer>> readOrderMenus() {
         writer.writeLine(MENU_INPUT_MESSAGE);
-        List<String> orderItems = splitOrderItems(reader.get());
 
-        return orderItems.stream()
-                .map(menuItem -> Map.entry(toMenuName(menuItem), toOrderCount(menuItem)))
+        List<String> orderItems = splitOrderItems(reader.get());
+        List<List<String>> orderMenus = orderItems.stream()
+                .map(this::splitOrderItem)
+                .toList();
+
+        return orderMenus.stream()
+                .map(orderMenu ->
+                        Map.entry(orderMenu.get(MENU_NAME_INDEX), toOrderCount(orderMenu.get(MENU_COUNT_INDEX))))
                 .toList();
     }
 
-    private List<String> splitOrderItems(String input) {
-        return Arrays.asList(input.split(ORDER_ITEM_DELIMITER));
+    private List<String> splitOrderItems(String orderItems) {
+        return Arrays.asList(orderItems.split(ORDER_ITEM_DELIMITER));
     }
 
-    private String toMenuName(String orderItem) {
-        return orderItem.split(ORDER_MENU_COUNT_DELIMITER)[0];
+    private List<String> splitOrderItem(String orderItem) {
+        List<String> orderMenuAndCount = Arrays.asList(orderItem.split(ORDER_MENU_COUNT_DELIMITER));
+        if (orderMenuAndCount.size() != MENU_ITEM_INDEX_COUNT) {
+            throw new IllegalArgumentException(INVALID_MENU_ERROR.format());
+        }
+
+        return orderMenuAndCount;
     }
 
-    private Integer toOrderCount(String orderItem) {
+    private Integer toOrderCount(String menuCount) {
         try {
-            String orderCount = orderItem.split(ORDER_MENU_COUNT_DELIMITER)[1];
-            return Integer.parseInt(orderCount);
+            return Integer.parseInt(menuCount);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INVALID_MENU_ERROR.format());
         }
