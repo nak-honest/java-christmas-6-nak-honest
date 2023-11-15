ff2722b3930cb155a55ec72c5350ea45e1a71a5b
nhlee98 <e5slnh9876@gmail.com>
2023-11-15 17:57:49 +0900

    fix: "[ERROR]" 중복 출력 제거

--- src/main/java/christmas/ErrorMessage.java
+++ src/main/java/christmas/ErrorMessage.java
@@ -3,7 +3,7 @@ package christmas;
 public enum ErrorMessage {
     INVALID_DAY_ERROR("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
     INVALID_MENU_ERROR("유효하지 않은 주문입니다. 다시 입력해 주세요."),
-    NEGATIVE_MONEY_ERROR("[ERROR] 금액은 음수일 수 없습니다.");
+    NEGATIVE_MONEY_ERROR("금액은 음수일 수 없습니다.");
 
     public static final String HEADER = "[ERROR]";
 
